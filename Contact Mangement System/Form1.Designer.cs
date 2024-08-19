5141f5a1de51dcf8b6fa16e7c813768640ed9487
SA-eyt <bscs23f10@namal.edu.pk>
2024-08-18 19:52:46 -0700

    Initial commit of Contact Management System

---
+++ Contact Mangement System/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿using YourNamespace;
+namespace Contact_Management_System
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDisplayAll = new System.Windows.Forms.Button();
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblName.Location = new System.Drawing.Point(50, 50);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 23);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+
+            this.btnDisplayAll.Click += new System.EventHandler(this.btnDisplayAll_Click);
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.listBoxContacts.SelectedIndexChanged += new System.EventHandler(this.listBoxContacts_SelectedIndexChanged);
+
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhoneNumber.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(50, 100);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(100, 23);
+            this.lblPhoneNumber.TabIndex = 1;
+            this.lblPhoneNumber.Text = "Phone Number";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEmail.Location = new System.Drawing.Point(50, 150);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(58, 23);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAddress.Location = new System.Drawing.Point(50, 200);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(78, 23);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtName.Location = new System.Drawing.Point(200, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 30);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhoneNumber.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtPhoneNumber.Location = new System.Drawing.Point(200, 100);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(200, 30);
+            this.txtPhoneNumber.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtEmail.Location = new System.Drawing.Point(200, 150);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 30);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtAddress.Location = new System.Drawing.Point(200, 200);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 30);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(50, 250);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnEdit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(150, 250);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 35);
+            this.btnEdit.TabIndex = 9;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(250, 250);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 35);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(350, 250);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 35);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDisplayAll
+            // 
+            this.btnDisplayAll.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnDisplayAll.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplayAll.ForeColor = System.Drawing.Color.White;
+            this.btnDisplayAll.Location = new System.Drawing.Point(450, 250);
+            this.btnDisplayAll.Name = "btnDisplayAll";
+            this.btnDisplayAll.Size = new System.Drawing.Size(100, 35);
+            this.btnDisplayAll.TabIndex = 12;
+            this.btnDisplayAll.Text = "Display All";
+            this.btnDisplayAll.UseVisualStyleBackColor = false;
+            this.btnDisplayAll.Click += new System.EventHandler(this.btnDisplayAll_Click);
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.ItemHeight = 16;
+            this.listBoxContacts.Location = new System.Drawing.Point(50, 300);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(500, 150);
+            this.listBoxContacts.TabIndex = 13;
+            this.listBoxContacts.SelectedIndexChanged += new System.EventHandler(this.listBoxContacts_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.listBoxContacts);
+            this.Controls.Add(this.btnDisplayAll);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Contact Management System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDisplayAll;
+        private System.Windows.Forms.ListBox listBoxContacts;
+    }
+}
---
+++ Contact Mangement System/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Contact_Management_System
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=contacts.db;Version=3;";
+        private List<Contact> contacts = new List<Contact>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            LoadContacts();
+        }
+
+        private void InitializeDatabase()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS Contacts (Id INTEGER PRIMARY KEY, Name TEXT, PhoneNumber TEXT, Email TEXT, Address TEXT)";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Contact newContact = new Contact()
+            {
+                Name = txtName.Text,
+                PhoneNumber = txtPhoneNumber.Text,
+                Email = txtEmail.Text,
+                Address = txtAddress.Text
+            };
+            AddContactToDatabase(newContact);
+            MessageBox.Show("Contact added successfully!");
+            ClearTextBoxes();
+            LoadContacts();
+        }
+
+        private void AddContactToDatabase(Contact contact)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Contacts (Name, PhoneNumber, Email, Address) VALUES (@Name, @PhoneNumber, @Email, @Address)";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.Parameters.AddWithValue("@Name", contact.Name);
+                command.Parameters.AddWithValue("@PhoneNumber", contact.PhoneNumber);
+                command.Parameters.AddWithValue("@Email", contact.Email);
+                command.Parameters.AddWithValue("@Address", contact.Address);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedItem != null)
+            {
+                int index = listBoxContacts.SelectedIndex;
+                Contact editedContact = contacts[index];
+                editedContact.Name = txtName.Text;
+                editedContact.PhoneNumber = txtPhoneNumber.Text;
+                editedContact.Email = txtEmail.Text;
+                editedContact.Address = txtAddress.Text;
+                UpdateContactInDatabase(editedContact);
+                MessageBox.Show("Contact edited successfully!");
+                ClearTextBoxes();
+                LoadContacts();
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to edit.");
+            }
+        }
+
+        private void UpdateContactInDatabase(Contact contact)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "UPDATE Contacts SET Name = @Name, PhoneNumber = @PhoneNumber, Email = @Email, Address = @Address WHERE Id = @Id";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.Parameters.AddWithValue("@Name", contact.Name);
+                command.Parameters.AddWithValue("@PhoneNumber", contact.PhoneNumber);
+                command.Parameters.AddWithValue("@Email", contact.Email);
+                command.Parameters.AddWithValue("@Address", contact.Address);
+                command.Parameters.AddWithValue("@Id", contact.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedItem != null)
+            {
+                int index = listBoxContacts.SelectedIndex;
+                Contact deletedContact = contacts[index];
+                DeleteContactFromDatabase(deletedContact);
+                contacts.RemoveAt(index);
+                MessageBox.Show("Contact deleted successfully!");
+                ClearTextBoxes();
+                LoadContacts();
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to delete.");
+            }
+        }
+
+        private void DeleteContactFromDatabase(Contact contact)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "DELETE FROM Contacts WHERE Id = @Id";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.Parameters.AddWithValue("@Id", contact.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchTerm = txtSearch.Text; // Assuming txtSearch is a TextBox where users input search terms
+            listBoxContacts.Items.Clear(); // Clear existing items
+            foreach (var contact in SearchContacts(searchTerm)) // Assuming SearchContacts() searches based on the input
+            {
+                listBoxContacts.Items.Add(contact);
+            }
+        }
+        private void listBoxContacts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedItem != null)
+            {
+                var selectedContact = (Contact)listBoxContacts.SelectedItem; // Assuming Contact is your contact class
+                                                                             // Display selected contact details
+                txtName.Text = selectedContact.Name; // Assuming txtName is a TextBox for the contact's name
+                txtPhoneNumber.Text = selectedContact.PhoneNumber; // Similarly for phone number
+                                                                   // Add other fields as necessary
+            }
+        }
+
+
+        private void btnDisplayAll_Click(object sender, EventArgs e)
+        {
+            // Example code to display all contacts
+            listBoxContacts.Items.Clear(); // Clear existing items
+            foreach (var contact in GetAllContacts()) // Assuming GetAllContacts() fetches contacts from your data source
+            {
+                listBoxContacts.Items.Add(contact);
+            }
+        }
+
+
+        private void LoadContacts()
+        {
+            contacts.Clear();
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Contacts";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Contact contact = new Contact()
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Name = reader["Name"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        Address = reader["Address"].ToString()
+                    };
+                    contacts.Add(contact);
+                }
+            }
+            listBoxContacts.DataSource = null;
+            listBoxContacts.DataSource = contacts;
+            listBoxContacts.DisplayMember = "Name";
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtName.Clear();
+            txtPhoneNumber.Clear();
+            txtEmail.Clear();
+            txtAddress.Clear();
+        }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
